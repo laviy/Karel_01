04bdacb078b1075ed1ccbaf61096c8fdc9a849a0
laviy <yonatan_lavi@asl.org>
2014-10-30 13:24:47 +0000

    lab01

--- Lab01.java
+++ Lab01.java
@@ -12,6 +12,71 @@ import edu.fcps.karel2.Robot;
 public class Lab01 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/school.map");
+   Display.setSize(10, 10);
+   Display.setSpeed(3);
+   Robot lisa = new Robot();
+   Robot pete = new Robot(4, 5, Display.SOUTH, 0);
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.pickBeeper();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.putBeeper();
+   pete.pickBeeper();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.putBeeper();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   
      }
 }
